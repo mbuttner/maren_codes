1d108cd3cfce2733e31a01ed68e6bd4d0fd05abd
mbuttner <maren.buettner@helmholtz-muenchen.de>
2018-04-18 13:07:29 +0200

    update method for scanpy 1.0.1

--- genes_to_xls.py
+++ genes_to_xls.py
@@ -4,10 +4,10 @@ def genes_to_xls(adata=None, filename='genes_hvg_regressed.xlsx'):
     import xlsxwriter
     import pandas as pd
     writer = pd.ExcelWriter(filename, engine='xlsxwriter')
-    for i in adata.uns[ 'rank_genes_groups_gene_names'].dtype.names:
-        d = {'gene':adata.uns['rank_genes_groups_gene_names'][i],
-             'score':adata.uns['rank_genes_groups_gene_scores'][i], 
-             'mean':adata.var['mean'][adata.uns['rank_genes_groups_gene_names'][i]]} #note the mean comes with an index.
+    for i in adata.uns['rank_genes_groups']['names'].dtype.names:
+        d = {'gene':adata.uns['rank_genes_groups']['names'][i],
+             'score':adata.uns['rank_genes_groups']['scores'][i], 
+             'mean':adata.var['mean'][adata.uns['rank_genes_groups']['names'][i]]} #note the mean comes with an index.
         df = pd.DataFrame(data=d)
         df.to_excel(writer,sheet_name=i)
     writer.save()
